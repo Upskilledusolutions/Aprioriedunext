72e74e7bb9495e9d3d08602d23b368c2f26c1b0b
Pushkargaur10 <gaurpushkar10@gmail.com>
2024-10-11 18:57:22 +0530

    23

--- src/Data/Languagelessons/mltilingual.js
+++ src/Data/Languagelessons/mltilingual.js
@@ -1,5 +1,19 @@
 export const data = [
 {
+    id:'1',
+    name:'Lesson 1',
+    level:'Beginners',
+    pdf:'https://drive.google.com/file/d/11iwjYA414cXNrzPL3qw6xVp0iHQufRrO/preview',
+    desc:'Multilingual Foundation Lesson 1 by Upskilledu Solutions is designed for beginners who are just starting their journey in learning Multiple languages. This foundational lesson introduces key vocabulary, essential phrases, and basic grammar concepts to help learners build a strong base in the language.',
+},
+{
+    id:'2',
+    name:'Lesson 2',
+    level:'Beginners',
+    pdf:'https://drive.google.com/file/d/1wsZGOsQg2OMaIn3GR2lLBAU1DalX1T8X/preview',
+    desc:'Multilingual Foundation Lesson 2 by Upskilledu Solutions is designed for beginners who are just starting their journey in learning Multiple languages. This foundational lesson introduces key vocabulary, essential phrases, and basic grammar concepts to help learners build a strong base in the language.',
+},
+{
     id:'3',
     name:'Lesson 3',
     level:'Beginners',
