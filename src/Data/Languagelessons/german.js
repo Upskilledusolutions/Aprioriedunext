b15519ab1741d0f6660a50156d8f67b67b313672
Pushkargaur10 <gaurpushkar10@gmail.com>
2024-10-14 15:33:38 +0530

    23

--- src/Data/Languagelessons/german.js
+++ src/Data/Languagelessons/german.js
@@ -23,14 +23,14 @@ export const data = [{
     id:'4',
     name:'Lesson 4',
     level:'Beginners',
-    pdf:'https://drive.google.com/file/d/1xo-WYbvNAXFQXgViH9Er6vbvh-GaK3zD/preview',
+    pdf:'https://drive.google.com/file/d/100lfn6s5HXIsD7y8C0JsLNG6mhm4qMa1/preview',
     desc:'German Foundation Lesson 4 by Upskilledu Solutions is designed for beginners who are just starting their journey in learning German. This foundational lesson introduces key vocabulary, essential phrases, and basic grammar concepts to help learners build a strong base in the language.',
 },
 {
     id:'5',
     name:'Lesson 5',
     level:'Beginners',
-    pdf:'https://drive.google.com/file/d/13XmkceTbJJzhhzHYx4zEabyB6Jks84IL/preview',
+    pdf:'https://drive.google.com/file/d/1BgcPBDYjbxZvc9X_igslaShweBhNw5Si/preview',
     desc:'German Foundation Lesson 5 by Upskilledu Solutions is designed for beginners who are just starting their journey in learning German. This foundational lesson introduces key vocabulary, essential phrases, and basic grammar concepts to help learners build a strong base in the language.',
 },
 {
