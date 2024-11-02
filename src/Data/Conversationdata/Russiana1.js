848edb01a1aa85a40b7c0c7628efad836e552a23
Pushkargaur10 <gaurpushkar10@gmail.com>
2024-11-02 11:16:29 +0530

    23

--- src/Data/Conversationdata/Russiana1.js
+++ src/Data/Conversationdata/Russiana1.js
@@ -2,7 +2,7 @@ export const youdata = [
     {
       id: '1',
       url: "/youtube/RuCA1.png",
-      youtube: "https://www.youtube.com/embed/HEeBWKon-5M?si=JalQNdKJaqZHyT93",
+      youtube: "https://www.youtube.com/embed/zV5klZalYoI?si=0tJ3rrZWAIeh59fn",
       title: "Russian Conversation 1",
       desc: "Welcome to our first French conversation video! 🎉 In this episode, we dive into a simple and practical conversation between two French speakers.",
     },
