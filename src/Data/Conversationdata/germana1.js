102b75a6ece3bf0289cc0c60fd6aa9a3ed033f02
Pushkargaur10 <gaurpushkar10@gmail.com>
2024-11-08 17:52:08 +0530

    23

--- src/Data/Conversationdata/germana1.js
+++ src/Data/Conversationdata/germana1.js
@@ -2,7 +2,7 @@ export const youdata = [
     {
       id: '1',
       url: "/youtube/GrCA1.png",
-      youtube: "https://www.youtube.com/embed/95aAEwblFsk?si=LVOevtDukjIbNqsF",
+      youtube: "https://www.youtube.com/embed/5XWevSXjprs?si=6KSqkJXBdB0sQAjt",
       title: "German Conversation 1",
       desc: "Welcome to our first German conversation video! 🎉 In this episode, we dive into a simple and practical conversation between two German speakers.",
     },
--- src/Data/Conversationdata/spanisha1.js
+++ src/Data/Conversationdata/spanisha1.js
@@ -2,7 +2,7 @@ export const youdata = [
   {
     id: '1',
     url: "/youtube/SpCA1.png",
-    youtube: "https://www.youtube.com/embed/XgEsW1KFXZs?si=YNzJWIwmo3-XsETi",
+    youtube: "https://www.youtube.com/embed/JuwOmuEQc68?si=rR-DCo6wiZSuwZvN",
     title: "Spanish Conversation 1",
     desc: "Welcome to our first Spanish conversation video! 🎉 In this episode, we dive into a simple and practical conversation between two Spanish speakers.",
   },
