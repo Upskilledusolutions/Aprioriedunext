d6af30e095327f053f8dc05e2119c26ca86339cb
Hyperglot1865 <seemasethi1685@gmail.com>
2024-11-21 11:48:23 +0530

    23

--- src/Data/Languagelessons/french.js
+++ src/Data/Languagelessons/french.js
@@ -207,6 +207,9 @@ name:'French Lesson 10 (A2)',
 level:'Beginners',
 pdf:'https://drive.google.com/file/d/1mHqkt8lQHmJJdA42NBNxQtkBjsv8jTxN/preview',
 desc:'French Foundation Lesson 10 by Upskilledu Solutions is designed for beginners who are just starting their journey in learning French. This foundational lesson introduces key vocabulary, essential phrases, and basic grammar concepts to help learners build a strong base in the language.',
-}
+},
+
 ]
+
+
 
