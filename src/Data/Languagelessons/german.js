db52ec256dfb9958b5e632b30ea2c9883cd4d209
Pushkargaur10 <gaurpushkar10@gmail.com>
2024-11-27 14:26:37 +0530

    23

--- src/Data/Languagelessons/german.js
+++ src/Data/Languagelessons/german.js
@@ -2,8 +2,8 @@ export const data = [{
     id:'1',
     name:'German Lesson 1 (A1)',
     level:'Beginners',
-    pdf:'https://drive.google.com/file/d/1Z4l9NwMos7_-FIzZdj-jmFtZssRghRo4/preview',
-    video:'https://www.youtube.com/embed/wSHmIk5D5_M?si=CK5lVzWWJdljT4sa',
+    pdf:'https://drive.google.com/file/d/1hUNNDlLbmlD-K_tP6o9AaSkWiWFsMAEi/preview',
+    video:'https://www.youtube.com/embed/pZm10iuCOBk?si=naBymzEeUtCABn4q',
     desc:'German Foundation Lesson 1 by Upskilledu Solutions is designed for beginners who are just starting their journey in learning German. This foundational lesson introduces key vocabulary, essential phrases, and basic grammar concepts to help learners build a strong base in the language.',
 },
 {
