360161b3784551e78fd1d45acef0ba5d2bc36028
Upskilledusolutions <web@upskilleduonline.com>
2026-09-23 12:42:36 +0530

    Send session credentials for protected status checks

--- components/ProtectedRoute.js
+++ components/ProtectedRoute.js
@@ -34,6 +34,7 @@ export default function ProtectedRoute({ children }) {
       try {
         const response = await fetch(`${URL}/api/check-status`, {
           method: "POST",
+          credentials: "include",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ userId: user.userId }),
         });
